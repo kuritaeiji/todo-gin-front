db7bb5d8567fed4e724daf7935abe73591d680c4
kuritaeiji <eiji199825@yahoo.co.jp>
2022-04-01 22:09:27 +0900

    toppage

--- layouts/toppage.vue
+++ layouts/toppage.vue
@@ -3,7 +3,17 @@
     <before-logged-in-app-bar />
     <v-main>
       <flash />
-      toppage
+      <v-container>
+        <v-row justify="center" class="pt-100">
+          <v-card-title class="text-h2 font-weight-bold mb-5">
+            {{ $t('appTitle') }}
+          </v-card-title>
+
+          <v-card-text class="text-center text-h6">
+            {{ $t('appTitle') }}は、Todoリストを作成できるwebアプリです。
+          </v-card-text>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
